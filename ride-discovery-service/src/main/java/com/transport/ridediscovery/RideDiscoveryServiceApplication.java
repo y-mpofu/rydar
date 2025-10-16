b286c1bcd742d744b40eb3f85f5beb4c9be51e4d
uosondu <uosondu@wesleyan.edu>
2025-10-16 16:11:34 -0400

    Add project setup for backend in Java,  and ignore IDE/build files

---
+++ ride-discovery-service/src/main/java/com/transport/ridediscovery/RideDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.transport.ridediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ ride-discovery-service/src/test/java/com/transport/ridediscovery/RideDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport.ridediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
